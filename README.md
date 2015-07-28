ba9712ee50c191bc8ab191e2a831d893772ab075
Ian Barwick <ian@2ndquadrant.com>
2015-07-28 12:16:01 +0900

    Add script to easily rename the extension

--- README.md
+++ README.md
@@ -7,6 +7,19 @@ This extension can be built as-is, but will only result in the
 installation of a single, useless function.
 
 
+Renaming the extension
+----------------------
+
+You'll probably want to rename the extension to something more useful;
+use the supplied script thusly:
+
+    ./rename.sh new_extension_name
+
+This will convert occurrences of the original exension name in the relevant
+source files, then rename the files appropriately. No checks are performed
+on the validity of the name.
+
+
 Useful links:
 -------------
 
---
+++ rename.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# Use this script to rename the extension to something
+# more useful; provide the new name as first argument
+# (it must be a valid PostgreSQL name string; this is not
+# checked!)
+
+if [ $# -lt 1 ]; then
+  echo "Please provide the new extension name as first argument"
+  exit 1
+fi
+
+NEW_NAME=$1
+
+# Convert occurences of 'extension_template' in source files to the new
+# name (do this first while we're sure what the files are)
+
+FILES=(
+  extension_template--0.1.sql
+  extension_template.c
+  extension_template.control
+  Makefile
+)
+
+for FILE in ${FILES[@]}; do
+  sed -i "s/extension_template/$NEW_NAME/" $FILE
+done
+
+# Rename files as appropriate
+
+rename extension_template $NEW_NAME extension_template*
+
