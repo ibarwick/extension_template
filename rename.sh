75230b8a0dfa8dddf14078192d100e6f45737f54
Ian Barwick <barwick@gmail.com>
2019-06-15 19:08:21 +0900

    Improve rename script
    - attempt to find a GNU-like "sed"
    - explicitly list files to be renamed (alas there is no really portable
      command for bulk renames)

--- rename.sh
+++ rename.sh
@@ -12,6 +12,19 @@ fi
 
 NEW_NAME=$1
 
+# Attempt to find a GNU-like sed
+SED=`which gsed 2>/dev/null`
+
+if [ "$SED" = "" ]; then
+	SED=`which sed 2>/dev/null`
+fi
+
+if [ "$SED" = "" ]; then
+    echo "unable to find sed"
+    exit 1
+fi
+
+
 # Convert occurences of 'extension_template' in source files to the new
 # name (do this first while we're sure what the files are)
 
@@ -19,14 +32,19 @@ FILES=(
   extension_template--0.1.sql
   extension_template.c
   extension_template.control
+  test/sql/extension_template.sql
+  test/expected/extension_template.out
   Makefile
 )
 
 for FILE in ${FILES[@]}; do
-  sed -i "s/extension_template/$NEW_NAME/g" $FILE
+  $SED -i "s/extension_template/$NEW_NAME/g" $FILE
 done
 
 # Rename files as appropriate
 
-rename extension_template $NEW_NAME extension_template*
-
+mv extension_template.c $NEW_NAME.c
+mv extension_template--0.1.sql $NEW_NAME--0.1.sql
+mv extension_template.control $NEW_NAME.control
+mv test/sql/extension_template.sql test/sql/$NEW_NAME.sql
+mv test/expected/extension_template.out test/expected/$NEW_NAME.out
